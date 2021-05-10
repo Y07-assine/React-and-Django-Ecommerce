1c5e04bfd79bff53446d72516d2499fe8aeab3da
Y07-assine <yassinemerzouki.ym@gmail.com>
2021-05-10 17:06:40 +0000

    modifie handel  add-to-cart

---
+++ src/src/utils.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import {endpoint} from './Constant';
+
+export const authAxios = axios.create({
+    baseURL: endpoint,
+    headers:{
+        Authorization: `Token ${localStorage.getItem('token')}`
+    }
+});
